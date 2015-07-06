05ca220d4a40cd6eed221640b2d424b1e2990384
Rajni Kumari <send2rajni@gmail.com>
2015-07-06 16:24:17 +0530

    create and show category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,17 +1,27 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
 
   def create
+    #render plain: params[:category].inspect
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category
+    else
+      render 'new'
+    end
   end
 
   def new
+    @category = Category.new
   end
 
   def edit
   end
 
   def show
+    @category=Category.find(params[:id])
   end
 
   def update
@@ -19,4 +29,10 @@ class CategoriesController < ApplicationController
 
   def destroy
   end
+
+  private
+  def category_params
+    params.require(:category).permit(:name, :desc)
+  end
+
 end
