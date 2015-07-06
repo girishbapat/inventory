c86c2a0173450a2468d8987130c90d38af0928b3
unknown <RDXSZ0048@SZ058.rdx.lgc.com>
2015-07-06 19:58:55 +0530

    Delete Category.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -36,6 +36,10 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+
+    redirect_to categories_path
   end
 
   private
